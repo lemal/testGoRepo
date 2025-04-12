92b817c5286a6a36dc58622df8e20392aa80687a
lemal <lemal@example.com>
2025-04-12 12:17:02 +0300

    corrected package name from main to add

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
-package main
-
-import "fmt"
+package add
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func Add(a, b int) int {
 	return a + b
-}
-
-func main() {
-	fmt.Println(Add(1, 2))
 }
